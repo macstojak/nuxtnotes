3097788f8aa85e74da3008fbc22a5de5f8529961
Maciej Stojak <maciej.stojak89@wp.pl>
2025-03-02 21:42:18 +0100

    add alerts and validation

--- pages/register.vue
+++ pages/register.vue
@@ -11,22 +11,24 @@
                 to your account
             </p>
             <form @submit.prevent="submit">
-            <div class="mt-8">
-                <label for="email" class="mb-0.5 block text-zinc-300 font-normal text-sm">Email address</label>
-                <input type="email" v-model="email" class="block w-full bg-zinc-700 border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm " placeholder="you@example.com">
-            </div>
-            <div class="mt-6">
-                <label for="password" class="mb-0.5 block text-zinc-300 font-normal text-sm">Password</label>
-                <input type="password" v-model="password" class="block w-full bg-zinc-700 border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm " placeholder="*********************">
-            </div>
-             <!-- Signup button -->
-            <div>
-                <button class="w-full mt-4 bg-[#FFAC00] rounded-full px-4 py-2 text-sm font-bold flex justify-center items-center space-x-2">
-                    Sign up 
-                    <ArrowRight/>
-                </button>
-            </div>
-        <!-- /Signup button -->
+                <div class="mt-8">
+                    <label for="email" class="mb-0.5 block text-zinc-300 font-normal text-sm">Email address</label>
+                    <input id="email" type="email" v-model="email" class="block w-full bg-zinc-700 border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm " placeholder="you@example.com">
+                </div>
+                <div class="mt-6">
+                    <label for="password" class="mb-0.5 block text-zinc-300 font-normal text-sm">Password</label>
+                    <input id="password" type="password" v-model="password" class="block w-full bg-zinc-700 border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm " placeholder="*********************">
+                </div>
+                <!-- Signup button -->
+                <div>
+                    <button
+                        class="w-full mt-4 bg-[#FFAC00] rounded-full px-4 py-2 text-sm font-bold flex justify-center items-center space-x-2"
+                    >
+                        <span>Sign Up</span>
+                        <ArrowRight />
+                    </button>
+                </div>
+            <!-- /Signup button -->
         </form>
         </div>
        
@@ -40,20 +42,28 @@
 </template>
 
 <script setup>
+import Swal from "sweetalert2"
 const email = ref("");
 const password = ref("");
 
-async function submit(e) {
-    console.log('submit',email.value,password.value);
-    await $fetch('/api/user', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email.value,
-            password: password.value
-        })
-    });
+async function submit() {
+    try{
+        const response = await $fetch('/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            })
+        });
+        const {isConfirmed} = await Swal.fire("Success", "Account created successfully!", "success");
+        if(isConfirmed){
+            navigateTo('/');
+        }
+    } catch (error) {
+        Swal.fire("Error",error.response?._data?.message, "error");
+    }
 }
 </script>
--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,14 +1,43 @@
 import prisma from "~/lib/prisma";
 import bcrypt from "bcryptjs";
+import validator from "validator";
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event);
-    const salt = await bcrypt.genSalt(10);    
-    const saltPassword = await bcrypt.hash(body.password, salt);
-    await prisma.user.create({ data: {
-        email: body.email,
-        password: saltPassword,
-        salt: String(salt)
-    }});
-    return { user: await prisma.user.findFirst() }
+    try{
+        const body = await readBody(event);
+        if(!validator.isEmail(body.email)){
+            throw createError({
+                statusCode: 400,
+                message: "Email is not valid, please check your email"
+            })
+        };
+        if(!validator.isStrongPassword(body.password,{
+            minLength: 8,
+            minLowercase: 0,
+            minUppercase: 0,
+            minNumbers: 0,
+            minSymbols: 0,
+        })){
+            throw createError({
+                statusCode: 400,
+                message: "Password is not long enough, please check if it has at least 8 characters"
+            });
+        }  
+
+        const salt = await bcrypt.genSalt(10);    
+        const saltPassword = await bcrypt.hash(body.password, salt);
+        
+        await prisma.user.create({ data: {
+            email: body.email,
+            password: saltPassword,
+            salt: String(salt)
+        }});
+    } catch (error) {
+        if(error.code === 'P2002'){
+            throw createError({
+                statusCode: 409,
+                message: "Email already exists"
+            }) 
+        }
+    }
 })
